d852772707aa514556445ac1dad1a45743263f07
nm <nagamocha@gmail.com>
2022-11-10 10:25:40 +0300

    [Primitives] Print matrix + transpose

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,19 @@
 use std::fmt;
 
-struct Color {
-    red: u8,
-    green: u8,
-    blue: u8,
+struct Matrix(f32, f32, f32, f32);
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
+    }
 }
 
-impl fmt::Display for Color {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "RGB ({0},{1},{2}) 0x{0:02X}{1:02X}{2:02X}",
-            self.red, self.green, self.blue
-        )
-    }
+fn transpose(m: Matrix) -> Matrix {
+    Matrix(m.0, m.2, m.1, m.3)
 }
 
 fn main() {
-    for color in [
-        Color {
-            red: 128,
-            green: 255,
-            blue: 90,
-        },
-        Color {
-            red: 0,
-            green: 3,
-            blue: 254,
-        },
-        Color {
-            red: 0,
-            green: 0,
-            blue: 0,
-        },
-    ]
-    .iter()
-    {
-        println!("{}", *color);
-    }
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("Matrix:\n{}", matrix);
+    println!("Transpose:\n{}", transpose(matrix));
 }
