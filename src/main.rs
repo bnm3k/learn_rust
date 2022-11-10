6b7fa96e3388ec56aeab0cb3b29d2e103f54f410
nm <nagamocha@gmail.com>
2022-11-10 13:14:05 +0300

    [Flow of Control] Do fizzbuzz

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,13 @@
-enum List {
-    Cons(u32, Box<List>),
-    Nil,
-}
-
-impl List {
-    fn new() -> List {
-        List::Nil
-    }
-
-    fn len(&self) -> u32 {
-        use List::*;
-        match *self {
-            Cons(_, ref tail) => 1 + tail.len(),
-            Nil => 0,
-        }
-    }
-
-    fn prepend(self, elem: u32) -> List {
-        List::Cons(elem, Box::new(self))
-    }
-
-    fn stringify(&self) -> String {
-        use List::*;
-        match *self {
-            Cons(head, ref tail) => {
-                format!("[{}] -> {}", head, tail.stringify())
-            }
-            Nil => format!("Nil"),
-        }
-    }
-}
 fn main() {
-    let l = List::new();
-    let l = l.prepend(1).prepend(2).prepend(3);
-    println!("l len={}", l.len());
-    println!("{}", l.stringify())
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz")
+        } else if n % 3 == 0 {
+            println!("fizz")
+        } else if n % 5 == 0 {
+            println!("buzz")
+        } else {
+            println!("{}", n)
+        }
+    }
 }
