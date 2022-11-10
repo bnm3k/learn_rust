e8bb9ce85e90aa982a584fd7b6cfee2e6fc25373
nm <nagamocha@gmail.com>
2022-11-10 11:47:50 +0300

    [Custom Types] Implement linked-list

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,38 @@
-#[derive(Debug)]
-struct Point {
-    x: f32,
-    y: f32,
+enum List {
+    Cons(u32, Box<List>),
+    Nil,
 }
 
-#[derive(Debug)]
-struct Rectangle {
-    top_left: Point,
-    bottom_right: Point,
-}
+impl List {
+    fn new() -> List {
+        List::Nil
+    }
 
-fn rect_area(r: Rectangle) -> f32 {
-    let Rectangle {
-        top_left: Point { x: sx, y: by },
-        bottom_right: Point { x: bx, y: sy },
-    } = r;
-    (bx - sx) * (by - sy)
-}
+    fn len(&self) -> u32 {
+        use List::*;
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0,
+        }
+    }
 
-fn square(top_left: Point, length: f32) -> Rectangle {
-    let Point { x: sx, y: by } = top_left;
-    Rectangle {
-        top_left: top_left,
-        bottom_right: Point {
-            x: sx + length,
-            y: by - length,
-        },
+    fn prepend(self, elem: u32) -> List {
+        List::Cons(elem, Box::new(self))
+    }
+
+    fn stringify(&self) -> String {
+        use List::*;
+        match *self {
+            Cons(head, ref tail) => {
+                format!("[{}] -> {}", head, tail.stringify())
+            }
+            Nil => format!("Nil"),
+        }
     }
 }
-
 fn main() {
-    let r = square(Point { x: 1.0, y: 9.0 }, 8.0);
-    println!("{:?}", r);
-    println!("area={}", rect_area(r));
+    let l = List::new();
+    let l = l.prepend(1).prepend(2).prepend(3);
+    println!("l len={}", l.len());
+    println!("{}", l.stringify())
 }
