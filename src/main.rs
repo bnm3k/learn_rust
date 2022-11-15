aff78421ab9eb78cc4415170af97f80c08467ce4
nm <nagamocha@gmail.com>
2022-11-15 14:44:33 +0300

    Implement struct for file handling

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,76 @@
-use clap::{App, Arg};
-use regex::Regex;
-use std::fs::File;
-use std::io;
-use std::io::prelude::*;
-use std::io::BufReader;
+#![allow(unused_variables)]
+#![allow(dead_code)]
 
-fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
-    for line_ in reader.lines() {
-        let line = &line_.unwrap();
-        match re.find(&line) {
-            Some(_) => println!("{}", line),
-            None => (),
+use rand::random;
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
+
+static mut ERROR: i32 = 0;
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
         }
+    }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+        let mut f = File::new(name);
+        f.data = data.clone();
+        f
+    }
+    fn open(&mut self) -> bool {
+        true
+    }
+
+    fn read(&self, save_to: &mut Vec<u8>) -> usize {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        read_length
+    }
+    fn close(&mut self) -> bool {
+        if random() && random() && random() {
+            unsafe {
+                ERROR = 1;
+            }
+        }
+        true
     }
 }
 
 fn main() {
-    let pattern_arg = Arg::with_name("pattern")
-        .help("The pattern to search for")
-        .takes_value(true)
-        .required(true);
-    let input_arg = Arg::with_name("input")
-        .help("File to search")
-        .takes_value(true)
-        .required(true);
-    let args = App::new("grep-lite")
-        .version("0.1")
-        .about("searches for patterns")
-        .arg(pattern_arg)
-        .arg(input_arg)
-        .get_matches();
+    let f1_data = vec![114, 117, 115, 116, 33];
+    let mut f1 = File::new_with_data("f1.txt", &f1_data);
 
-    // pattern
-    let pattern = args.value_of("pattern").unwrap();
-    let re = Regex::new(pattern).unwrap();
+    let mut buffer: Vec<u8> = vec![];
+    f1.open();
 
-    // input
-    let input = args.value_of("input").unwrap();
-
-    // let ctx_lines = 2;
-    if input == "-" {
-        let stdin = io::stdin();
-        let reader = stdin.lock();
-        process_lines(reader, re);
-    } else {
-        let f = File::open(input).unwrap();
-        let reader = BufReader::new(f);
-        process_lines(reader, re);
+    // read
+    let f1_length = f1.read(&mut buffer);
+    unsafe {
+        if ERROR != 0 {
+            panic!("An error has occured while reading the file")
+        }
     }
+
+    // close
+    f1.close();
+    unsafe {
+        if ERROR != 0 {
+            panic!("An error has occured while closing the file")
+        }
+    }
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f1);
+    println!("{} is {} bytes long", &f1.name, f1_length);
+    println!("{}", text);
 }
