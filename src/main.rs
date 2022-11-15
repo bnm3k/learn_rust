ea650723f0f70578c2b4fe4c745466911b85861c
nm <nagamocha@gmail.com>
2022-11-15 13:02:22 +0300

    Implement grep-lite

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,51 @@
-use num::complex::Complex;
+use clap::{App, Arg};
+use regex::Regex;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
 
-fn calculate_mandelbrot(
-    max_iters: usize,
-    x_min: f64,
-    x_max: f64,
-    y_min: f64,
-    y_max: f64,
-    width: usize,
-    height: usize,
-) -> Vec<Vec<usize>> {
-    let mut rows: Vec<_> = Vec::with_capacity(width);
-    for img_y in 0..height {
-        let mut row: Vec<usize> = Vec::with_capacity(height);
-        for img_x in 0..width {
-            let x_percent = img_x as f64 / width as f64;
-            let y_percent = img_y as f64 / height as f64;
-            let cx = x_min + (x_max - x_min) * x_percent;
-            let cy = y_min + (y_max - y_min) * y_percent;
-            let escaped_at = mandelbrot_at_point(cx, cy, max_iters);
-            row.push(escaped_at);
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = &line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
         }
-        rows.push(row);
-    }
-    rows
-}
-
-fn mandelbrot_at_point(cx: f64, cy: f64, max_iters: usize) -> usize {
-    let mut z = Complex { re: 0.0, im: 0.0 };
-    let c = Complex::new(cx, cy);
-    for i in 0..=max_iters {
-        if z.norm() > 2.0 {
-            return i;
-        }
-        z = z * z + c;
-    }
-    max_iters
-}
-
-fn render_mandelbrot(escape_vals: Vec<Vec<usize>>) {
-    for row in escape_vals {
-        let mut line = String::with_capacity(row.len());
-        for column in row {
-            let val = match column {
-                0..=2 => ' ',
-                2..=5 => '.',
-                5..=10 => '•',
-                11..=30 => '*',
-                30..=100 => '+',
-                100..=200 => 'x',
-                200..=400 => '$',
-                400..=700 => '#',
-                _ => '%',
-            };
-            line.push(val);
-        }
-        println!("{}", line);
     }
 }
 
 fn main() {
-    let mandelbrot = calculate_mandelbrot(1000, 2.0, 1.0, -1.0, 1.0, 100, 24);
-    render_mandelbrot(mandelbrot);
+    let pattern_arg = Arg::with_name("pattern")
+        .help("The pattern to search for")
+        .takes_value(true)
+        .required(true);
+    let input_arg = Arg::with_name("input")
+        .help("File to search")
+        .takes_value(true)
+        .required(true);
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(pattern_arg)
+        .arg(input_arg)
+        .get_matches();
+
+    // pattern
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+
+    // input
+    let input = args.value_of("input").unwrap();
+
+    // let ctx_lines = 2;
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
+    }
 }
