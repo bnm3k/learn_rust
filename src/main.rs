1cc4a0ca32c9481aa2efecd1a3928b77aa79ea94
nm <nagamocha@gmail.com>
2022-11-17 17:44:10 +0300

    [Generics] Explore generics

--- src/main.rs
+++ src/main.rs
@@ -2,50 +2,97 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
-fn apply_transform<F>(mut f: F)
-where
-    F: FnMut(&mut i32),
-{
-    let mut val: i32 = 1;
-    println!("val={val}");
-    f(&mut val);
-    println!("val={val}");
+use std::fmt::{Debug, Display};
+
+struct Point<T> {
+    x: T,
+    y: T,
 }
 
-fn add_10(v: &mut i32) {
-    *v += 10;
+impl<T: Debug + Display> Point<T> {
+    fn print(&self) {
+        println!("Point(x: {}, y: {})", self.x, self.y);
+    }
 }
 
-fn create_adder(n: i32) -> impl FnMut(&mut i32) {
-    let add_fn = move |v: &mut i32| {
-        *v += n;
-    };
-    return add_fn;
+struct Empty;
+struct Null;
+
+trait HasNothing {
+    fn is(&self, s: &str);
 }
 
-fn other() {
-    let x = 7;
-    let add_7 = |v: &mut i32| {
-        *v += x;
-    };
-    let add_20 = create_adder(20);
-    apply_transform(add_7);
-    apply_transform(add_10);
-    apply_transform(add_20);
+impl HasNothing for Empty {
+    fn is(&self, val: &str) {
+        println!("Is Empty: {val}")
+    }
+}
+
+impl HasNothing for Null {
+    fn is(&self, val: &str) {
+        println!("Is Null: {val}")
+    }
+}
+
+impl<T> HasNothing for Point<T> {
+    fn is(&self, val: &str) {
+        println!("Is Point: {val}")
+    }
+}
+
+trait DoubleDrop<T> {
+    fn double_drop(self, _: T);
+}
+
+impl<T: HasNothing, U: HasNothing> DoubleDrop<T> for U {
+    fn double_drop(self, t: T) {
+        self.is("self");
+        t.is("t");
+    }
+}
+// ---------------------------------------------------------------------------
+struct Pair(i32, i32);
+
+trait Contains {
+    type A;
+    type B;
+    fn contains(&self, _: &Self::A, _: &Self::B) -> bool;
+    fn first(&self) -> i32;
+    fn last(&self) -> i32;
+}
+
+impl Contains for Pair {
+    type A = i32;
+    type B = i32;
+
+    fn contains(&self, n1: &i32, n2: &i32) -> bool {
+        (&self.0 == n1) && (&self.1 == n2)
+    }
+    fn first(&self) -> i32 {
+        self.0
+    }
+    fn last(&self) -> i32 {
+        self.1
+    }
+}
+
+fn difference<C: Contains>(container: &C) -> i32 {
+    container.last() - container.first()
 }
 
 fn main() {
-    for n in 1..101 {
-        if n % 15 == 0 {
-            println!("fizzbuzz")
-        } else if n % 3 == 0 {
-            println!("fizz")
-        } else if n % 5 == 0 {
-            println!("buzz")
-        } else {
-            println!("{}", n)
-        }
-    }
-    let vec1 = vec![1, 2, 3];
-    println!("2 in vec: {}", vec1.into_iter().any(|n| n == 2));
+    // let p = Point { x: 10.0, y: 20.0 };
+    // p.print();
+    // p.print();
+    // let empty = Empty;
+    // let null = Null;
+    // empty.double_drop(null);
+    // null.double_drop(empty);
+    // p.double_drop(empty);
+    // empty;
+    // null;
+    //
+    let n1 = 3;
+    let n2 = 10;
+    let container = Pair(n1, n2);
 }
