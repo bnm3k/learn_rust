933224d10db263bf90996e485fafe30a68d86e5f
nm <nagamocha@gmail.com>
2022-11-25 23:41:49 +0300

    Use cargo workspaces

---
+++ add_one/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add_one(10);
+        assert_eq!(result, 11);
+    }
+}
