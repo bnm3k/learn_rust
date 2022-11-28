0db9b97eea176f0bd1d49b5b345bf5f0c3ff9513
nm <nagamocha@gmail.com>
2022-11-28 13:37:43 +0300

    Implement Multiple producers, single consumer

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,26 @@
 use std::sync::mpsc;
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    let (ping_tx, ping_rx) = mpsc::channel();
-    let (pong_tx, pong_rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel();
+    let num_producers = 10;
+    for id in 1..=num_producers {
+        let tx = tx.clone();
+        let _producer = thread::spawn(move || {
+            let id_str = format!("p-{}", id);
+            if let Err(e) = tx.send(id_str) {
+                eprintln!("send error on producer {id}: {e}");
+            }
+        });
+    }
 
-    let pinger = thread::spawn(move || {
-        let msg = "Ping";
-        for _ in 0..10 {
-            thread::sleep(Duration::from_secs(1));
-            if let Err(e) = ping_tx.send(msg) {
-                eprintln!("Send error: {e}");
-            }
-            match pong_rx.recv() {
-                Ok(received) => {
-                    println!("{}", received);
-                }
-                Err(e) => {
-                    eprintln!("Receive error: {e}");
-                }
-            }
-        }
-    });
-    let ponger = thread::spawn(move || {
-        let msg = "Pong";
-        for _ in 0..5 {
-            let received = ping_rx.recv().unwrap();
-            println!("{}", received);
-            thread::sleep(Duration::from_secs(1));
-            pong_tx.send(msg).unwrap();
+    std::mem::drop(tx);
+
+    let consumer = thread::spawn(move || {
+        for msg in rx {
+            println!("{}", msg);
         }
     });
 
-    pinger.join().unwrap();
-    ponger.join().unwrap();
+    consumer.join().unwrap();
 }
