15465ba4becf467597476ac073b709bdcefa5d7a
nm <nagamocha@gmail.com>
2022-11-29 00:55:26 +0300

    Implement multi-threaded worker pool for handling http requests

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,24 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        handle_conection(stream);
+        pool.execute(|| {
+            handle_conection(stream);
+        });
     }
+
+    println!("Shutting down");
 }
 
 fn handle_conection(mut stream: TcpStream) {
@@ -22,10 +31,13 @@ fn handle_conection(mut stream: TcpStream) {
         .collect();
 
     let request_line = &http_request[0];
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "static/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "static/404.html")
+    let (status_line, filename) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "static/hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "static/hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "static/404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
