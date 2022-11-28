750d3b43c2e11490957f71dc7cb780a932ca51cc
nm <nagamocha@gmail.com>
2022-11-28 14:24:54 +0300

    Use Mutex

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
+use std::time::Duration;
 
 fn main() {
+    let mut handles = vec![];
     let (tx, rx) = mpsc::channel();
     let num_producers = 10;
+    let counter = Arc::new(Mutex::new(0));
+
     for id in 1..=num_producers {
         let tx = tx.clone();
-        let _producer = thread::spawn(move || {
-            let id_str = format!("p-{}", id);
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            thread::sleep(Duration::from_millis(1));
+            let count = {
+                let mut num = counter.lock().unwrap();
+                *num += 1;
+                num
+            };
+            let id_str = format!("p-{} -> {}", id, count);
             if let Err(e) = tx.send(id_str) {
                 eprintln!("send error on producer {id}: {e}");
             }
         });
+        handles.push(handle);
     }
 
     std::mem::drop(tx);
@@ -21,6 +33,10 @@ fn main() {
             println!("{}", msg);
         }
     });
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 
     consumer.join().unwrap();
 }
